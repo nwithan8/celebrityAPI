2e575304f9c70a8b27644e0c7989c27213ff2533
nharris <nharris@easypost.com>
2022-09-27 00:26:04 -0600

    - Return JSON for deaths listings

--- main.py
+++ main.py
@@ -24,30 +24,33 @@ async def celebrity_details(imdb_id: str):
 
 
 @app.get("/celebrity/deaths/this_week")
-async def celebrity_deaths_this_week() -> Dict:
+async def celebrity_deaths_this_week() -> List[Dict]:
     start_of_week_date = start_of_this_week()
     end_of_week_date = end_of_this_week()
     deaths: List[CelebrityBrief] = await imdb.get_celebrity_deaths(start_date=start_of_week_date,
                                                                    end_date=end_of_week_date)
-
-    return {"message": f"Hello Deaths"}
+    if not deaths:
+        return []
+    return [death.to_json() for death in deaths]
 
 
 @app.get("/celebrity/deaths/this_month")
-async def celebrity_deaths_this_month() -> Dict:
+async def celebrity_deaths_this_month() -> List[Dict]:
     start_of_month_date = start_of_this_month()
     end_of_month_date = end_of_this_month()
     deaths: List[CelebrityBrief] = await imdb.get_celebrity_deaths(start_date=start_of_month_date,
                                                                    end_date=end_of_month_date)
-
-    return {"message": f"Hello Deaths 2"}
+    if not deaths:
+        return []
+    return [death.to_json() for death in deaths]
 
 
 @app.get("/celebrity/deaths/last_week")
-async def celebrity_deaths_last_week() -> Dict:
+async def celebrity_deaths_last_week() -> List[Dict]:
     start_of_week_date = start_of_week_offset(offset=-1)
     end_of_week_date = end_of_week_offset(offset=-1)
     deaths: List[CelebrityBrief] = await imdb.get_celebrity_deaths(start_date=start_of_week_date,
                                                                    end_date=end_of_week_date)
-
-    return {"message": f"Hello Deaths 3"}
+    if not deaths:
+        return []
+    return [death.to_json() for death in deaths]
